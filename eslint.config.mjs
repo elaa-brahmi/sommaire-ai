d5ae5b012153f1c9af83b6262a5172523a952f84
elaa <elaa.brahmii@gmail.com>
2026-02-11 12:40:18 +0100

    disable eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
-import { dirname } from "path";
+/* import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
@@ -23,3 +23,4 @@ const eslintConfig = [
 ];
 
 export default eslintConfig;
+ */
